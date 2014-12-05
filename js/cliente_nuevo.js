af7f5ab46512e5d6952d38254547eb7fb239b5db
Alan Alvarenga <alankvm@gmail.com>
2014-12-04 22:21:40 -0600

    validacion de campos

--- js/cliente_nuevo.js
+++ js/cliente_nuevo.js
@@ -1,7 +1,14 @@
 Cliente = {
 	datosGenerales: {},
+	tipoPersona: {},
+	nit: {},
+	dui: {},
+	telefono_1: {},
+	telefono_2: {},
+	celular: {},
+	fax: {},
 	clienteValido: function() {
-		datosGenerales.validate({
+		Cliente.datosGenerales.validate({
 			rules: {
 				nit: {
 					required: true
@@ -28,9 +35,38 @@ Cliente = {
 
 		});
 	},
+	cambiarTipoPersona: function () {
+		console.log("entre");	
+		var tipoSeleccionado = Cliente.tipoPersona.find("option:selected").text(),
+			tipoNoSeleccionado = Cliente.tipoPersona.find("option").not(":selected").text();
+
+		$("div[data-grupo]").each( function(index, input) {
+			var grupoInput = $(input).data("grupo");
+			if(String(tipoSeleccionado).toLowerCase() == grupoInput) {
+				$(input).show();
+				Cliente.nit.mask("9999-999999-999-9");
+				Cliente.dui.mask("99999999-9");
+			} else {
+				$(input).hide();
+			}
+
+		});
+	},
 	init: function() {
-		datosGenerales = $("#nuevo_cliente");
+		this.datosGenerales = $("#nuevo_cliente");
+		this.tipoPersona = $("#tipo_persona");
+		this.nit = $("#nit");
+		this.dui = $("#dui");
+		this.telefono_1 = $("#telefono_1");
+		this.telefono_2 = $("#telefono_2");
+		this.celular = $("#celular");
 		Cliente.clienteValido();
+		this.tipoPersona.on("change", Cliente.cambiarTipoPersona);
+		this.nit.mask("9999-999999-999-9");
+		this.dui.mask("99999999-9");
+		this.telefono_1.mask("9999-9999");
+		this.telefono_2.mask("9999-9999");
+		this.celular.mask("9999-9999");
 
 	}
 
